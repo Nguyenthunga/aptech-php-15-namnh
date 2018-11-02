cc9407e073b8682f8a3283c5a826ae42f2385d60
[name] <namnh.website@gmail.com>
2018-11-02 20:34:43 +0700

    updated ss 6

---
+++ php/6-2018-11-02/route-view/app/Http/Controllers/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function hello()
+    {
+        $users = DB::table('users')->get();
+        // $roles = DB::table('roles')->get();
+        return view('welcome', ['users' => $users]);
+    }
+}
